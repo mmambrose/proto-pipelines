42841b5a593bd9ec0b1f61f95c0aa6b65926232c
Maggie Ambrose <mambrose@pivotal.io>
2017-02-27 16:35:29 -0600

    try new file path

--- tasks/prepare-release.sh
+++ tasks/prepare-release.sh
@@ -5,6 +5,7 @@ set -e
 cp -a proto-pipelines/. prepare-release/
 cp -a java-app-manifest/. prepare-release/packer-code/roles/files/
 cp -a java-app-release/. prepare-release/packer-code/roles/files/
+cp -a java-app-release/. prepare-release/packer-code/roles/
 
 cat <<EOF > prepare-release/secrets.json
 { $CONFIG }
