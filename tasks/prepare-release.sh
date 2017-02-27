840c54a6d8a5faf89233c22e4aac76fff5539495
Maggie Ambrose <mambrose@pivotal.io>
2017-02-27 09:33:40 -0600

    add ansible playbook

--- tasks/prepare-release.sh
+++ tasks/prepare-release.sh
@@ -3,6 +3,8 @@
 set -e
 
 cp -a proto-pipelines/. prepare-release/
+cp -a java-app-manifest/. prepare-release/
+cp -a java-app-release/. prepare-release/
 
 cat <<EOF > prepare-release/secrets.json
 { $CONFIG }
