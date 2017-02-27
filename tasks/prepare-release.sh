34dfb8bd623a82ad7ba2ca2cfc81f60e06b847c5
Maggie Ambrose <mambrose@pivotal.io>
2017-02-27 16:22:39 -0600

    adding jar file and directory structure

--- tasks/prepare-release.sh
+++ tasks/prepare-release.sh
@@ -3,8 +3,8 @@
 set -e
 
 cp -a proto-pipelines/. prepare-release/
-cp -a java-app-manifest/. prepare-release/
-cp -a java-app-release/. prepare-release/
+cp -a java-app-manifest/. prepare-release/packer-code/roles/files/
+cp -a java-app-release/. prepare-release/packer-code/roles/files/
 
 cat <<EOF > prepare-release/secrets.json
 { $CONFIG }
