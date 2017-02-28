bb06deaea3ab1fda60b4912245b3f5ed5f907d7e
Maggie Ambrose <mambrose@pivotal.io>
2017-02-27 18:19:31 -0600

    add role

--- tasks/prepare-release.sh
+++ tasks/prepare-release.sh
@@ -3,9 +3,8 @@
 set -e
 
 cp -a proto-pipelines/. prepare-release/
-cp -a java-app-manifest/. prepare-release/packer-code/roles/files/
-cp -a java-app-release/. prepare-release/packer-code/roles/files/
-cp -a java-app-release/. prepare-release/packer-code/roles/
+cp -a java-app-manifest/. prepare-release/roles/files/
+cp -a java-app-release/. prepare-release/roles/files/
 
 cat <<EOF > prepare-release/secrets.json
 { $CONFIG }
