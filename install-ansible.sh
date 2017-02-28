b8a6f05c9e8ee9472370691fe9dd3410e75928dc
Maggie Ambrose <mambrose@pivotal.io>
2017-02-28 17:53:25 -0600

    sudo start task

--- install-ansible.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-sudo apt-get -y install software-properties-common
-sudo apt-add-repository ppa:ansible/ansible
-sudo apt-get -y update
-sudo apt-get -y install ansible
-find .
